ae66373c538b74fd83ed7756482425a52a0a612e
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2025-05-09 17:19:39 +0530

    Implement Firebase authentication with user registration and login functionality

---
+++ lib/auth/auth.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final _auth = FirebaseAuth.instance;
+
+  Future<User?> createUserWithEmaiAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      final credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } catch (e) {
+      log("account create failed");
+    }
+  }
+
+  Future<User?> logWithEmailAndPassword(String email, String password) async {
+    try {
+      final credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return credential.user;
+    } catch (e) {
+      log("user login failed");
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      log("something went wrong");
+    }
+  }
+}
